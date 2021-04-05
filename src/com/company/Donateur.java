06966ebe6544e08422a99d13163f90474c7e84a8
Jean-Michel Depaepe <jedepaepe@epfc.eu>
2021-04-05 13:13:50 +0200

    Update Donateur.java
    don oublié

--- src/com/company/Donateur.java
+++ src/com/company/Donateur.java
@@ -3,9 +3,9 @@ package com.company;
 import java.util.ArrayList;
 
 public class Donateur extends Personne{
-    private ArrayList<Produit> don;
+    private ArrayList<Produit> don; // le donateur peut faire plusieurs dons => ArrayList<Don> dons (cette classe manque)
 
-    public Donateur(String nom, String prenom, String telephone, String email_adresse, ArrayList<Produit> don) {
+    public Donateur(String nom, String prenom, String telephone, String email_adresse, ArrayList<Produit> don) {    // retirer don du constructeur (vous ne connaissez pas la liste des dons au moment où vous créer le donateur)
         super(nom, prenom, telephone, email_adresse);
         this.don = don;
     }
@@ -13,4 +13,6 @@ public class Donateur extends Personne{
     public ArrayList<Produit> getDon() {
         return don;
     }
+    
+    // ajouter une méthode addDon(Don don) {...} 
 }
